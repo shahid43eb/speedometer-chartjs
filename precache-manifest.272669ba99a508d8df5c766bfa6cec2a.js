b13ac54fd4a97175534451304d57bda6a4eca935
Shahid <shahid.rasheed@arbisoft.com>
2021-04-13 18:26:26 +0500

    Updates

---
+++ precache-manifest.272669ba99a508d8df5c766bfa6cec2a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0a37c9b030e876aec246758e658a15",
+    "url": "/speedometer-charjs/index.html"
+  },
+  {
+    "revision": "d3a640d8735f6bb78a27",
+    "url": "/speedometer-charjs/static/js/2.85a304aa.chunk.js"
+  },
+  {
+    "revision": "cf4c97738db3c3eddf14",
+    "url": "/speedometer-charjs/static/js/main.b27f8729.chunk.js"
+  },
+  {
+    "revision": "2a69d33cb97206b7fb2b",
+    "url": "/speedometer-charjs/static/js/runtime~main.96f07333.js"
+  }
+]);
